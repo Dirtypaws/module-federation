fc4a870b2e573db76626f25ada03d4aa9241d254
Matthew Krizanac - Home <matthewkrizanac@gmail.com>
2024-10-08 14:37:12 -0500

    Home commit

--- frontend/core/src/app/app.component.ts
+++ frontend/core/src/app/app.component.ts
@@ -1,26 +1,17 @@
 import { Component, signal } from '@angular/core';
-import { getManifest } from './app.routes';
 
 @Component({
   selector: 'app-root',
+  styleUrl: './app.component.scss',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
   sidebarVisible = signal(false);
   navLinks: { path: string; display: string }[] = [];
 
-  constructor() {
-    const manifest = getManifest();
-    Object.keys(manifest).map((key) => {
-      const entry = manifest[key];
-      this.navLinks.push({
-        path: entry.routePath,
-        display: entry.displayName,
-      });
-    });
-  }
+  constructor() {}
 
-  openSidebar(state: boolean): void {
+  toggleSidebar(state: boolean): void {
     this.sidebarVisible.set(!state);
   }
 }
