b40aa2fe8184e3379e077cce6aee5421aadeba36
Matthew Krizanac - Home <matthewkrizanac@gmail.com>
2024-10-26 19:46:06 -0500

    cleanup and nesting

--- utils/openapi-gen/src/cs/dotnet.ts
+++ utils/openapi-gen/src/cs/dotnet.ts
@@ -2,16 +2,7 @@ import * as fs from 'fs';
 
 export class Dotnet {
   static createController(controller: string, method: string, output: string, project: string): void {
-    const filePath = `${output}/I${controller}.cs`;
-    if (fs.existsSync(filePath)) {
-      const file = fs.readFileSync(filePath, 'utf-8');
-      if (file.indexOf(`public static async Task<IResult> ${method}()`)) {
-        return;
-      }
-
-      const closingBraceIdx = file.lastIndexOf('}');
-    }
-
+    const filePath = `${output}/I${controller}.g.cs`;
     const code = `
 using ${project}.Client.Models;
 
@@ -28,11 +19,6 @@ public interface I${controller}
   static registerProgram(project: string, path: string, verbs: string[]) {
     const controller = path.substring(1).charAt(0).toUpperCase() + path.slice(2);
     const filePath = `${controller}.Program.g.cs`;
-    if (fs.existsSync(filePath)) {
-      fs.unlink(filePath, (err) => {
-        console.log(`Unable to delete file at: ${filePath}`);
-      });
-    }
 
     let code = `
 using ${project}.Controllers;
